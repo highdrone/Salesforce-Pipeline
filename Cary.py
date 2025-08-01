5e56397408c1b6a16c7c460d7379daa9d4efe259
Mr. E <iamthecolorist@gmail.com>
2025-08-01 11:21:09 -0400

    Updated sales forecast calculations in Cary.py to use current month data instead of year data, enhancing accuracy in metrics reporting.

--- Cary.py
+++ Cary.py
@@ -318,10 +318,10 @@ def calculate_metrics(df):
         (filtered_opportunities['Close Date'].dt.year == current_year)
     ]
     
-    # Sales forecast by type - using same filtering as pipeline (ForecastCategory + date range)
-    project_forecast = filtered_opportunities[filtered_opportunities['Type'] == 'System']['Amount'].sum()
-    trans_forecast = filtered_opportunities[filtered_opportunities['Type'] == 'Transactional']['Amount'].sum()
-    service_forecast = filtered_opportunities[filtered_opportunities['Type'] == 'Service']['Amount'].sum()
+    # Sales forecast by type - using same filtering as pipeline (ForecastCategory + current month)
+    project_forecast = sales_by_type[sales_by_type['Type'] == 'System']['Amount'].sum()
+    trans_forecast = sales_by_type[sales_by_type['Type'] == 'Transactional']['Amount'].sum()
+    service_forecast = sales_by_type[sales_by_type['Type'] == 'Service']['Amount'].sum()
     
     # Sales MTD by type - using same filtering as pipeline (current month)
     project_mtd = sales_by_type[sales_by_type['Type'] == 'System']['Amount'].sum()
